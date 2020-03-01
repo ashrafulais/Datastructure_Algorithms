4efc628a2a60de3dca25aeaf3889e2347be893e8
Ashraful Islam <ashraful0103@gmail.com>
2020-03-01 16:01:49 +0600

    Linkedlist, binary search added

---
+++ CsharpDataStructure/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace CsharpDataStructure
+{
+    class Algoritms
+    {
+        public int BinarySearch(int[] numbers, int findNumber)
+        {
+            int arrayLength, startIndex, midIndex, endIndex, numPosition;
+
+            arrayLength = numbers.Length;
+
+            numPosition = -1;
+            startIndex = 0;
+            endIndex = arrayLength-1;
+
+            while(endIndex >= startIndex)
+            {
+                midIndex = ((startIndex + endIndex) / 2);
+
+                //midIndex +- 1 is done for skipping computational deadlocks
+                //1, 2, 3, 4, 5, 6, 7, 8, 9
+                //findinf 9 will fall in deadlock in 7th position
+                
+                if(findNumber == numbers[midIndex])
+                {
+                    numPosition = midIndex;
+                    break;
+                }
+                else if(findNumber < numbers[midIndex])
+                {
+                    endIndex = midIndex-1;
+                }
+                else
+                {
+                    startIndex = midIndex+1;
+                }
+            }
+
+
+            return numPosition;
+        }
+        public int BinarySearchRecursive(int[] numbers, int findNumber, int startIndex, int endIndex)
+        {
+            //Console.WriteLine("length: {0} start: {1} end:{2}", numbers.Length, startIndex, endIndex);
+            int midIndex;
+
+            if (endIndex >= startIndex)
+            {
+                midIndex = (startIndex + endIndex) / 2;
+
+                if (findNumber == numbers[midIndex])
+                {
+                    return midIndex;
+                }
+
+                else if (findNumber < numbers[midIndex])
+                {
+                    return BinarySearchRecursive(numbers, findNumber, 0, midIndex-1);
+                }
+                else
+                {
+                    return BinarySearchRecursive(numbers, findNumber, midIndex+1, endIndex);
+                }
+            }
+            return -1;
+        }
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*
+             Shortcut for inline commenting
+             >>to comment: crtl+k+c
+             >>to uncomment: ctrl+k+u
+            */
+
+            //THIS IS ONE OF THE WAY TO INPUT AN ARRAY
+            /*
+            int num_size, i;
+            string []input_str;
+
+            //Read Input
+            Console.Write("How many numbers?: ");
+            num_size = Convert.ToInt32(Console.ReadLine());
+
+            int[] numbers = new int[num_size];
+
+            //Read The Numbers
+            Console.Write("Please input {0} numbers: ", num_size);
+            input_str = Console.ReadLine().Split(" ");
+
+            //Make an array of ints'
+            for(i=0; i<num_size; i++)
+            {
+                numbers[i] = Int32.Parse(input_str[i]);
+            }
+
+            Console.Write("The numbers are: ");
+            for(i=0; i<num_size; i++)
+            {
+                Console.Write(" {0}", numbers[i]);
+            }
+            
+            */
+
+            //THIS IS THE SHORT WAY WAY TO INPUT AN ARRAY
+            int i, arraySize, findNumber, findIndex;
+            int[] numbers = { 2, 3, 4, 10, 40 };
+            SearchAlgoritms searcherObj = new SearchAlgoritms();
+
+            //Input array Formalities
+            /*
+            Console.Write("Input the numbers with a space: ");
+            numbers = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+
+            arraySize = numbers.Length;
+            
+            Console.WriteLine("Size of the array: {0}", arraySize);
+
+            Console.Write("The numbers are: ");
+            for (i = 0; i < arraySize; i++)
+            {
+                Console.Write(" {0}", numbers[i]);
+            }
+            
+
+            Console.Write("\nWhat number to find: ");
+            findNumber = Convert.ToInt32(Console.ReadLine());
+            */
+
+            //TASK-------------------------------------------
+            //Implement a sorting function in another project
+
+            foreach (int tempNum in numbers)
+            {
+                //findIndex = searcherObj.BinarySearch(numbers, tempNum-1);
+                findIndex = searcherObj.BinarySearchRecursive(numbers, tempNum, 0, numbers.Length - 1);
+                Console.Write("\nNumber {0} in Position: {1}",tempNum, findIndex);
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ DataStructure/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructure
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+    }
+
+    public class LinkedList
+    {
+        public Node Head;
+        public LinkedList()
+        {
+            Head = null;
+        }
+        
+        public Node GetHead()
+        {
+            if (Head == null)
+            {
+                Head = new Node();
+            }
+            return Head;
+        }
+        
+        public void Insert(int val)
+        {
+            Console.WriteLine($"Inserting {val}");
+
+            Node newNode = new Node();
+            newNode.Data = val;
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node Current = Head;
+                Node Parent;
+                while(true)
+                {
+                    Parent = Current;
+                    Current = Current.Next;
+
+                    if(Current == null)
+                    {
+                        Parent.Next = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+
+        public void ShowAll()
+        {
+            Console.WriteLine("\n>Reading all data from the LinkedList:");
+            Node Current = Head;
+            if (Current != null)
+            {
+                while (Current != null)
+                {
+                    Console.WriteLine(Current.Data);
+                    Current = Current.Next;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Empty linked list");
+            }
+        }
+
+        public void Delete(int val)
+        {
+
+            Console.WriteLine("\n>Deleting {0}", val);
+            //strategy
+            //Head.Next = Head.Next.Next;
+
+            Node Current = Head;
+            Node Previous = null;
+
+            while (Current != null)
+            {
+                if (Current.Data == val)
+                {
+                    Current = Current.Next;
+                    Previous.Next = Current;
+
+                    Console.WriteLine("Deletion Successful");
+                    return;
+                }
+                Previous = Current;
+                Current = Current.Next;
+            }
+            Console.WriteLine("Deletion Failed");
+        }
+
+        public void Edit(int findValue, int modifiedValue)
+        {
+            Console.WriteLine("\n>Updating {0} to {1}", findValue, modifiedValue);
+
+            Node Current = Head;
+
+            while(Current != null)
+            {
+                if(Current.Data == findValue)
+                {
+                    Current.Data = modifiedValue;
+                    Console.WriteLine("Successfully updated");
+                    break;
+                }
+                Current = Current.Next;
+            }
+            Console.WriteLine("Failed updating");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var List = new LinkedList();
+            var insertList = new List<int>() { 1, 3, 4, 6, 2, 0 };
+
+            //Inserting values
+            foreach(var val in insertList)
+            {
+                List.Insert(val);
+            }
+            List.Delete(2);
+            
+            //Showing values
+            List.ShowAll();
+
+            List.Edit(0, 7);
+
+            List.ShowAll();
+        }
+    }
+}
